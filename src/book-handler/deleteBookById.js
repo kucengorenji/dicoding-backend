8af04df953cf6b598a6db7c7f5e1fe37eaef644a
mikelrisyad <mikelrisyad@gmail.com>
2022-02-25 22:33:39 +0700

    feat: add handle delete, get all book, and detail

---
+++ src/book-handler/deleteBookById.js
@@ -0,0 +1,26 @@
+const books = require('../books');
+
+const deleteBookById = (request, h) => {
+  const { id } = request.params;
+
+  const index = books.findIndex((book) => book.id === id);
+
+  if (index !== -1) {
+    books.splice(index, 1);
+    const response = h.response({
+      status: 'success',
+      message: 'Buku berhasil dihapus',
+    });
+    response.code(200);
+    return response;
+  }
+
+  const response = h.response({
+    status: 'fail',
+    message: 'Buku gagal dihapus. Id tidak ditemukan',
+  });
+  response.code(404);
+  return response;
+};
+
+module.exports = deleteBookById;
---
+++ src/book-handler/getAllBooks.js
@@ -0,0 +1,10 @@
+const books = require('../books');
+
+const getAllBooks = () => ({
+  status: 'success',
+  data: {
+    books,
+  },
+});
+
+module.exports = getAllBooks;
